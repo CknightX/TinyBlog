8d5a91bc68425e971eaaba3d8d8c74870fe02e9b
CknightX <caokeck123@163.com>
2017-10-11 15:47:41 +0800

    add login

--- app/models.py
+++ app/models.py
@@ -1,27 +1,41 @@
-from flask.ext.sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import db, login_manager
 
-basedir=os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI']=\
-    'sqlite:///'+os.path.join(basedir,'data.sqlite')
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
-db=SQLAlchemy(app)
 
-# 定义数据模型
 class Role(db.Model):
-    __tablename__='roles' #数据库使用的表名
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(64),unique=True)
-    users=db.relationship('User',backref='role') #关系
-    def __repr__(self):
-        return '<Role %r>'%self.name
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
-class User(db.Model):
-    __tablename__='users'
-    id=db.Column(db.Integer,primary_key=True)
-    email=db.Column(db.String(64),unique=True,index=True)
-    username=db.Column(db.String(64),unique=True,index=True)
-    password_hash=db.Column(db.String(128))
-    role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))#关系
     def __repr__(self):
-        return '<User %r>'%self.username
+        return '<Role %r>' % self.name
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
